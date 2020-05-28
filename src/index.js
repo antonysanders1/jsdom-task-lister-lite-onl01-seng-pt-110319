ed91ed077290c1240cfcb9994ce1f0b8d4542011
Antony Sanders <antony.devon92@gmail.com>
2020-05-28 05:45:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //Selects the submit button
+  const tags = document.getElementsByTagName("input");
+    tags[1].addEventListener("click", function(event) {
+        event.preventDefault();
 });
