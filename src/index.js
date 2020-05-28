af1c64bcc74453b4b644633a4364a5a12f563d6f
Antony Sanders <antony.devon92@gmail.com>
2020-05-28 05:51:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  //Selects the submit button
+  
+  //Assigns the variable to the HTML element by Tag name
   const tags = document.getElementsByTagName("input");
+  
+  //Adds a click event listener to the soncond element with the 'input' tag name => the Submit button
     tags[1].addEventListener("click", function(event) {
+      // prevents the default action of redirect upon submission
         event.preventDefault();
         
         
